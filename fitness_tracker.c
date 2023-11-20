34d4c9fa1f4379ce6cd8fe0a2d97d70d42fe7267
NB1223 <148526314+NB1223@users.noreply.github.com>
2023-11-20 14:08:50 +0530

    Update fitness_tracker.c

--- fitness_tracker.c
+++ fitness_tracker.c
@@ -1,28 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-typedef struct goals
+typedef struct goals_steps
 {
-    int achieved;
-    int total;
-    struct goals *next;
+    int achieved_steps;
+    int total_steps;
+    struct goals_steps *next;
 
-} GOALS;
+}GOALS_S;
+
+typedef struct goals_hrs
+{
+    int achieved_hrs;
+    int total_hrs;
+    struct goals_hrs *next;
+
+}GOALS_H;
 
 typedef struct date
 {
     int day;
     int month;
     int year;
-} DATE;
+}DATE;
 
 typedef struct workout_log
 {
     DATE date;
     char workout_name[30];
     int hrs;
-
-} LOG;
+    
+}LOG;
 
 typedef struct nutrition
 {
@@ -31,7 +39,7 @@ typedef struct nutrition
     char dinner[30];
     double calories;
 
-} FOOD;
+}FOOD;
 
 typedef struct health_metric
 {
@@ -41,7 +49,7 @@ typedef struct health_metric
     char gender;
     int age;
     char name[20];
-} HM;
+}HM;
 
 typedef struct tracking_parameters
 {
@@ -52,7 +60,43 @@ typedef struct tracking_parameters
     int calories;
     DATE date;
 
-} TRACK;
+}TRACK;
+
+typedef struct challenges
+{
+    char chall_name[20];
+}CHALL;
+
+int main()
+{
+    char name[20];
+    printf("Please enter your name - "); scanf("%s", name);
+
+    printf("\t\t-------------------------\n\t\t\tWelcome %s !!\n", name);
+
+    int level;
+    printf("Choose Difficulty Level - \n1.Beginner \n2.Intermidiate \n3.Advanced\n ~"); scanf("%d", &level);
+
+    switch (level)
+    {
+    case 1:
+        printf("Here in beginner");
+        break;
+
+    case 2:
+        printf("Here in Intermidiate");
+        break;
+
+    case 3:
+        printf("Here in Advanced");
+        break;
+
+    default:
+        printf("Enter Valid Output");
+        break;
+    }    
+}
+
 
 int main()
 {
